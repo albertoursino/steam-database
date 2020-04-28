32755204c4e3bacb88a6ffd64ea5fd73bfb1d84e
Alberto Ursino <alberto.ursino@studentri.unipd.it>
2020-04-28 17:03:50 +0200

    Created a JDBC
    - Created the method "createConnection()"
    - Added a listener to the connection button "connection_btn"
    - Added the "response" JLabel in order to see if the connection went wrong or not -> It has to be reimplemented better
    - General changes
    - Updated .gitignore
    - Added JDBC lib

--- src/com/steamdb/SteamApp.java
+++ src/com/steamdb/SteamApp.java
@@ -2,6 +2,11 @@ package com.steamdb;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SteamApp {
 
@@ -10,21 +15,24 @@ public class SteamApp {
     }
 
     public void show() {
+        int row = 6;
+
         // Show the UI.
-        JFrame frame = new JFrame("steam db");
-        JLabel addLab = new JLabel("Indirizzo del server a cui connettersi: ");
-        JLabel portLab = new JLabel("Porta: ");
-        JLabel userLab = new JLabel("Nome utente: ");
+        JFrame frame = new JFrame("Steam Database");
+        JLabel addLab = new JLabel("IP Address: ");
+        JLabel portLab = new JLabel("Port: ");
+        JLabel userLab = new JLabel("Username: ");
         JLabel pwLab = new JLabel("Password: ");
-        JLabel dbLab = new JLabel("Nome del database: ");
+        JLabel dbLab = new JLabel("Database name: ");
         JTextField addText = new JTextField(12);
         JTextField portText = new JTextField(6);
         JTextField dbText = new JTextField(20);
         JTextField userText = new JTextField(20);
+        JLabel response = new JLabel();
         JPasswordField pwText = new JPasswordField(10);
-        JPanel[] rows = new JPanel[5];
+        JPanel[] rows = new JPanel[row];
 
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < row; i++) {
             rows[i] = new JPanel();
             rows[i].setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
         }
@@ -49,25 +57,47 @@ public class SteamApp {
         rows[4].add(pwLab);
         rows[4].add(pwText);
 
-        //ADD A BUTTON TO ESTABLISH CONNECTION
 
-        JButton bun = new JButton("Connettiti al database");
-        bun.setHorizontalAlignment(JButton.CENTER);
-        bun.setMargin(new Insets(5, 5, 5, 5));
-        bun.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        //ADD A BUTTON TO ESTABLISH CONNECTION
+        JButton connection_btn = new JButton("Connect");
+        connection_btn.setHorizontalAlignment(JButton.CENTER);
+        connection_btn.setMargin(new Insets(5, 5, 5, 5));
+        connection_btn.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+
+        response.setHorizontalAlignment(JLabel.CENTER);
+        response.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        connection_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    Class.forName("org.postgresql.Driver");
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Connection connection = createConnection(addText.getText(), portText.getText(), dbText.getText(), userText.getText(), String.valueOf(pwText.getPassword()));
+                    response.setText("Connection OK");
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                    response.setText("Something went wrong");
+                }
+            }
+        });
 
         //PUT EVERYTHING TOGETHER
         Box verticalBox = Box.createVerticalBox();
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < row; i++) {
             rows[i].setMaximumSize(new Dimension(Integer.MAX_VALUE, rows[i].getHeight()));
             verticalBox.add(rows[i], JComponent.LEFT_ALIGNMENT);
         }
 
         Component c = Box.createRigidArea(new Dimension(Integer.MAX_VALUE, 20));
         verticalBox.add(c);
-        verticalBox.add(bun);
-
+        verticalBox.add(connection_btn);
+        verticalBox.add(response);
         verticalBox.add(Box.createVerticalGlue());
+
         frame.add(verticalBox);
         frame.setSize(500, 500);
         frame.setLocationRelativeTo(null);
@@ -76,12 +106,28 @@ public class SteamApp {
     }
 
     public static void main(final String[] args) {
+        SteamApp steamApp = new SteamApp(args);
+
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new SteamApp(args).show();
+                steamApp.show();
             }
         });
+
+    }
+
+    /**
+     * @param address  of the database, can't be null
+     * @param port     of the databse, can't be null
+     * @param dbName   name of the database, can't be null
+     * @param username of user who is connection to the database, can't be null
+     * @param password of the user who is connection to the database, can't be null
+     * @return the {@link Connection} object
+     * @throws SQLException may be thrown if something wrong with the database happens
+     */
+    public static Connection createConnection(String address, String port, String dbName, String username, String password) throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://" + address + ":" + port + "/" + dbName, username, password);
     }
 }
